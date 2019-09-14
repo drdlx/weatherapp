28f4cb5bc4a387d50637c925b45237d2a1fa7073
Daniil Shelkovnikov <wired@fastmail.com>
2019-09-14 20:26:23 +0600

    Move API key to a different place

--- app/src/main/java/com/example/weatherapp/api/ApiClient.kt
+++ app/src/main/java/com/example/weatherapp/api/ApiClient.kt
@@ -1,13 +1,15 @@
 package com.example.weatherapp.api
 
+import android.content.res.Resources
+import android.provider.Settings.System.getString
 import org.json.JSONObject
 import android.util.Log
+import com.example.weatherapp.R
 
 class ApiClient {
 
     private val url = "http://api.openweathermap.org/data/2.5/weather"
-    private val open_weather_maps_app_id = "9576ca9040bc18b631e06442b76587d2"
-    //private val open_weather_maps_app_id = "11111"
+    private val open_weather_maps_app_id = Resources.getSystem().getString(R.string.api_key)
 
     fun requestByCityName(city: String): JSONObject? {
         Log.d("DEBUG", "City request by name!, city is $city")
@@ -20,8 +22,8 @@ class ApiClient {
                     "appid" to open_weather_maps_app_id
                 )
             )
-            Log.d("DEBUG", "${response.statusCode}")
-            Log.d("DEBUG", "${response.text}")
+            Log.d("DEBUG", response.statusCode.toString())
+            Log.d("DEBUG", response.text)
             if (response.statusCode != 200) {
                 Log.e("ERROR", response.statusCode.toString())
                 Log.e("ERROR", "city:$city")
